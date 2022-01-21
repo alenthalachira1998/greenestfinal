5d5d476895379f6af28f7b124237fe311da6f2cf
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-01-21 18:33:33 +0530

    check web

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
 
-    // plugins: {
-    //   'postcss-import': {},
-    //   tailwindcss: {},
-    //   'postcss-preset-env': { stage: 2 },
-    // },
+
+    plugins: {
+      'postcss-import': {},
+      autoprefixer: {},
+      tailwindcss: {},
+      'postcss-preset-env': { stage: 2 },
+    },
   };
 
