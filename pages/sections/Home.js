e86153fdc2c03d1481396f3faee417ddce5c973e
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-01-21 23:54:06 +0530

    check

--- pages/sections/Home.js
+++ pages/sections/Home.js
@@ -10,7 +10,7 @@ backgroundSize: 'cover',
 backgroundPosition: 'center',
 backgroundRepeat: 'no-repeat',
 height:'90vh',
-height:'-webkit-fill-available;'
+
 
 }
 
@@ -18,11 +18,11 @@ return (
 <section id="home">
 
 <div className=" relative" style={bg}>
-<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center" style={{height:'90vh', height:'-webkit-fill-available;'}}></div>
+<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center" style={{height:'100vh'}}></div>
 <div className="relative ">
-<div className="flex-col flex   text-center h-screen">
+<div className="flex-col flex   text-center " style={{height:'90vh'}}>
 
-<div className=" text-gray-900  mt-36 lg:text-3xl  text-sm  lg:font-normal flex flex-col items-center justify-center  ">
+<div className=" text-gray-900  mt-36 lg:text-3xl  text-sm  lg:font-normal flex flex-col items-center justify-center  " >
 &quot;Lets build a Home <br/> that sings with the rain<br/> hums with the Earth<br/> and slieving the sun for night.&quot;<br/>
 <h1 className="text-gray-900 lg:text-4xl text-xl font-bold mt-6 lg:mt-10">Buildings that breathe</h1>
 <img  className=" lg:mt-10 mt-6 lg:w-60 z-30 w-36 drop-shadow-xl "src="/images/OneLogo.png"/>
