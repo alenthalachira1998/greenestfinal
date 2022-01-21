5fa8b1f426ecf00189d6886c8130ff1e80d85832
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-01-21 23:26:01 +0530

    check

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-
-
-  ]
-}
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    'postcss-preset-env': { stage: 2 },
+  },
+};
