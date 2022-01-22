208a1ef4a76de9400d8deb072160d7f62ef9cbc2
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-01-22 17:11:18 +0530

    check

--- pages/sections/ElegantResortLiving.js
+++ pages/sections/ElegantResortLiving.js
@@ -74,8 +74,8 @@ const ElegantResortLiving = () => {
     return (
         <>
             <section className="elegant-resort-living ">
-           <div className="flex flex-col  w-screen" style={{height:'100vh', height:'-webkit-fill-available;'}}>
-                <div className="flex  w-screen " style={{height:'100vh', height:'-webkit-fill-available;'}}>
+           <div className="flex flex-col  w-screen h-screen">
+                <div className="flex  w-screen h-screen ">
                     <div className=" lg:ml-4 hidden justify-center transform rotate-180 items-center vtext lg:flex  w-10 md:w-36">
                         <h1 className="text-gray-300 xl:font-bold 2xl:text-8xl xl:text-7xl sm:text-5xl md:text-6xl font-medium  text-center  ">
                             
--- pages/sections/Home.js
+++ pages/sections/Home.js
@@ -23,12 +23,12 @@ return (
 <div className="relative ">
 <div className="flex-col flex   text-center h-screen">
 
-<div className=" text-gray-900  mt-36 lg:text-3xl  text-sm  lg:font-normal flex flex-col items-center justify-center  " >
+<div className=" text-gray-900  mt-28 lg:36 lg:text-3xl  text-sm  lg:font-normal flex flex-col items-center justify-center  " >
 &quot;Lets build a Home <br/> that sings with the rain<br/> hums with the Earth<br/> and slieving the sun for night.&quot;<br/>
-<h1 className="text-gray-900 lg:text-4xl text-xl font-bold mt-6 lg:mt-10">Buildings that breathe</h1>
-<img  className=" lg:mt-10 mt-6 lg:w-60 z-30 w-36 drop-shadow-xl "src="/images/OneLogo.png"/>
+<h1 className="text-gray-900 lg:text-4xl text-base font-bold mt-2 lg:mt-10">Buildings that breathe</h1>
+<img  className=" lg:mt-10 mt-2 lg:w-60 z-30 w-24 drop-shadow-xl "src="/images/OneLogo.png"/>
 </div>
-<span className="text-white  z-20 absolute  text-center bottom-14 sm:bottom-4  w-screen  lg:text-8xl text-6xl  font-thin"><IoIosArrowRoundDown className="w-screen animate-bounce" /></span>
+<span className="text-white  z-20 absolute  text-center bottom-12 sm:bottom-4  w-screen  lg:text-8xl text-6xl  font-thin"><IoIosArrowRoundDown className="w-screen animate-bounce" /></span>
 </div>
 </div>
 
