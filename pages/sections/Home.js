faed9d9044c73ad465a69c17e2e505966b5e9ec4
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-01-22 16:53:29 +0530

    check

--- pages/sections/Home.js
+++ pages/sections/Home.js
@@ -19,16 +19,16 @@ return (
 <section id="home">
 <Div100vh>
 <div className=" relative" style={bg}>
-<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center" style={{height:'100vh'}}></div>
+<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center h-screen"></div>
 <div className="relative ">
-<div className="flex-col flex   text-center " style={{height:'100vh'}}>
+<div className="flex-col flex   text-center h-screen">
 
 <div className=" text-gray-900  mt-36 lg:text-3xl  text-sm  lg:font-normal flex flex-col items-center justify-center  " >
 &quot;Lets build a Home <br/> that sings with the rain<br/> hums with the Earth<br/> and slieving the sun for night.&quot;<br/>
 <h1 className="text-gray-900 lg:text-4xl text-xl font-bold mt-6 lg:mt-10">Buildings that breathe</h1>
 <img  className=" lg:mt-10 mt-6 lg:w-60 z-30 w-36 drop-shadow-xl "src="/images/OneLogo.png"/>
 </div>
-<span className="text-white  z-20 absolute  text-center bottom-16 sm:bottom-4  w-screen  lg:text-8xl text-6xl  font-thin"><IoIosArrowRoundDown className="w-screen animate-bounce" /></span>
+<span className="text-white  z-20 absolute  text-center bottom-14 sm:bottom-4  w-screen  lg:text-8xl text-6xl  font-thin"><IoIosArrowRoundDown className="w-screen animate-bounce" /></span>
 </div>
 </div>
 
