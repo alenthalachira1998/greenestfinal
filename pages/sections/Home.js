8e6d9d16381fef4f0010feda42d4bb6d734f3bc4
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-02-21 22:53:32 +0530

    rfgrg

--- pages/sections/Home.js
+++ pages/sections/Home.js
@@ -10,23 +10,18 @@ backgroundSize: 'cover',
 backgroundPosition: 'center',
 backgroundRepeat: 'no-repeat',
 height:'100vh',
-height: '-webkit-fill-available'
 
 
 
-}
-const ht = {   
-    height: '100vh',
-    height: '-webkit-fill-available'
 }
 
 return (
 <section id="home">
 
 <div className=" relative" style={bg}>
-<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center " style={ht}></div>
+<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center h-screen"></div>
 <div className="relative ">
-<div className="flex-col flex   text-center " style={ht}>
+<div className="flex-col flex   text-center h-screen">
 
 <div className=" text-gray-800 mt-36  3xl:mt-72 lg:text-xl 3xl:text-9xl desk:text-xl xl:mt-28  2xl:mt-32 tb:text-2xl  xl:text-xl  2xl:text-3xl  text-sm  2xl:font-normal flex flex-col items-center justify-center  " >
 &quot;Lets build a Home <br/> that sings with the rain<br/> hums with the Earth<br/> and sieving the sun, for night.&quot;<br/>
@@ -46,5 +41,3 @@ return (
 )
 }
 export default HomePageGreen;
-
-
