25a129ed3954bed3d6834dd978b456fd86240087
alenthalachira1998 <alenthalachira.official@gmail.com>
2022-02-21 21:06:38 +0530

    rfgrg

--- pages/sections/Home.js
+++ pages/sections/Home.js
@@ -1,48 +1,5 @@
 
 
-// import React from 'react'
-// import { IoIosArrowRoundDown } from 'react-icons/io';
-// import Iframe from "react-iframe"
-// const HomePageGreen = () => {
-// const bg = {
-// backgroundImage: 'url("/images/resort/Home.jpg")',
-// backgroundSize: 'cover',
-// backgroundPosition: 'center',
-// backgroundRepeat: 'no-repeat',
-// height:'100vh',
-
-
-
-// }
-
-// return (
-// <section id="home">
-
-// <div className=" relative" style={bg}>
-// <div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center h-screen"></div>
-// <div className="relative ">
-// <div className="flex-col flex   text-center h-screen">
-
-// <div className=" text-gray-800 mt-36  3xl:mt-72 lg:text-xl 3xl:text-9xl desk:text-xl xl:mt-28  2xl:mt-32 tb:text-2xl  xl:text-xl  2xl:text-3xl  text-sm  2xl:font-normal flex flex-col items-center justify-center  " >
-// &quot;Lets build a Home <br/> that sings with the rain<br/> hums with the Earth<br/> and sieving the sun, for night.&quot;<br/>
-// <h1 className="text-gray-800 xl:text-2xl 3xl:text-10xl  desk:text-2xl  2xl:text-3xl tb:text-2xl lg:text-xl text-base font-bold  mt-2 2xl:mt-4">Buildings that breathe</h1>
-// <img  className=" 2xl:mt-4 mt-2 2xl:w-52 3xl:w-60  desk:w-40 z-30 w-32 xl:w-40 tb:w-44 drop-shadow-xl "src="/images/OneLogo.png"/>
-// </div>
-// <span className="text-white  z-20 absolute   text-center bottom-12 sm:bottom-4  w-screen  2xl:text-8xl text-6xl  font-thin"><IoIosArrowRoundDown className="w-screen animate-bounce" /></span>
-// </div>
-// </div>
-
-
-// </div>
-
-
-
-// </section>
-// )
-// }
-// export default HomePageGreen;
-
-
 import React from 'react'
 import { IoIosArrowRoundDown } from 'react-icons/io';
 import Iframe from "react-iframe"
@@ -53,23 +10,18 @@ backgroundSize: 'cover',
 backgroundPosition: 'center',
 backgroundRepeat: 'no-repeat',
 height:'100vh',
-height:'-webkit-fill-available;'
 
 
-}
-ht={
-    height:'100vh',
-    height:'-webkit-fill-available;'
 
 }
 
 return (
-<section id="home overflow-hidden">
+<section id="home">
 
 <div className=" relative" style={bg}>
-<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center" style={ht}></div>
+<div className="absolute bg-black  w-full opacity-5 flex flex-col items-center justify-center h-screen"></div>
 <div className="relative ">
-<div className="flex-col flex   text-center " style={ht}>
+<div className="flex-col flex   text-center h-screen">
 
 <div className=" text-gray-800 mt-36  3xl:mt-72 lg:text-xl 3xl:text-9xl desk:text-xl xl:mt-28  2xl:mt-32 tb:text-2xl  xl:text-xl  2xl:text-3xl  text-sm  2xl:font-normal flex flex-col items-center justify-center  " >
 &quot;Lets build a Home <br/> that sings with the rain<br/> hums with the Earth<br/> and sieving the sun, for night.&quot;<br/>
@@ -89,3 +41,5 @@ return (
 )
 }
 export default HomePageGreen;
+
+
